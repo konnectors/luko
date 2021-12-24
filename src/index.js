eed8139d9a295622b91c407e79c65f9c907e1bdf
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2021-12-24 16:41:52 +0100

    feat: konnector up and running

--- src/index.js
+++ src/index.js
@@ -1,124 +1,109 @@
-const {
-  BaseKonnector,
-  requestFactory,
-  scrape,
-  log,
-  utils
-} = require('cozy-konnector-libs')
+const { BaseKonnector, requestFactory, log } = require('cozy-konnector-libs')
+
 const request = requestFactory({
-  // The debug mode shows all the details about HTTP requests and responses. Very useful for
-  // debugging but very verbose. This is why it is commented out by default
-  // debug: true,
-  // Activates [cheerio](https://cheerio.js.org/) parsing on each page
+  debug: false,
   cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
-  json: false,
-  // This allows request-promise to keep cookies between requests
+  json: true,
   jar: true
 })
 
-const VENDOR = 'template'
-const baseUrl = 'http://books.toscrape.com'
+const VENDOR = 'Luko'
+const baseUrl = 'https://fr.luko.eu'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
-// cozyParameters are static parameters, independents from the account. Most often, it can be a
-// secret api key.
 async function start(fields, cozyParameters) {
   log('info', 'Authenticating ...')
   if (cozyParameters) log('debug', 'Found COZY_PARAMETERS')
+
   await authenticate.bind(this)(fields.login, fields.password)
+
   log('info', 'Successfully logged in')
-  // The BaseKonnector instance expects a Promise as return of the function
   log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
-  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
+
   log('info', 'Parsing list of documents')
-  const documents = await parseDocuments($)
 
-  // Here we use the saveBills function even if what we fetch are not bills,
-  // but this is the most common case in connectors
+  const documents = await parseBills()
+  log('info', documents)
   log('info', 'Saving data to Cozy')
-  await this.saveBills(documents, fields, {
-    // This is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
-  })
-}
 
-// This shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
-function authenticate(username, password) {
-  return this.signin({
-    url: `http://quotes.toscrape.com/login`,
-    formSelector: 'form',
-    formData: { username, password },
-    // The validate function will check if the login request was a success. Every website has a
-    // different way to respond: HTTP status code, error message in HTML ($), HTTP redirection
-    // (fullResponse.request.uri.href)...
-    validate: (statusCode, $, fullResponse) => {
-      log(
-        'debug',
-        fullResponse.request.uri.href,
-        'not used here but should be useful for other connectors'
-      )
-      // The login in toscrape.com always works except when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
-        return true
-      } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
-        return false
-      }
+  await this.saveFiles(documents.downloadFiles, fields, {
+    linkBankOperations: false,
+    identifiers: ['Luko'],
+    sourceAccount: this.accountId,
+    sourceAccountIdentifier: fields.login,
+    contentType: 'application/pdf',
+    fileAttributes: {
+      contentAuthor: 'fr-luko.eu'
     }
   })
+  // await this.saveBills(documents, fields, {
+  //   linkBankOperations: false,
+  //   identifiers: ['Luko'],
+
+  // })
 }
 
-// The goal of this function is to parse a HTML page wrapped by a cheerio instance
-// and return an array of JS objects which will be saved to the cozy by saveBills
-// (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
-function parseDocuments($) {
-  // You can find documentation about the scrape function here:
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
-  const docs = scrape(
-    $,
-    {
-      title: {
-        sel: 'h3 a',
-        attr: 'title'
+async function authenticate(username, password) {
+  request(`${baseUrl}/`)
+
+  await request({
+    url: `https://cerbere.luko.eu/v1?lang=fr`,
+    method: 'POST',
+    json: {
+      operationName: 'Login',
+      variables: {
+        credentials: {
+          email: `${username}`,
+          password: `${password}`
+        }
       },
-      amount: {
-        sel: '.price_color',
-        parse: normalizePrice
-      },
-      fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
-      }
+      query:
+        'mutation Login($credentials: credentials!) {\n  login(cred: $credentials) {\nok\nuser {\nid\nemail\nfirstname\nlastname\nconnection {\njwtToken\nintercomTokens {\nweb\n}\n}\n }\n}\n}\n'
     },
-    'article'
-  )
-  return docs.map(doc => ({
-    ...doc,
-    // The saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
+    resolveWithFullResponse: true
+  })
+  // authResp = authResp.request.body
+  // log('info', JSON.parse(authResp))
+  // log('info', authResp.request.response.body.data.login.user.connection)
+}
+
+async function parseBills() {
+  request(`${baseUrl}/my-account/invoices`)
+
+  let authResp
+  authResp = await request({
+    url: `https://cerbere.luko.eu/v1?lang=fr`,
+    method: 'POST',
+    json: {
+      operationName: 'GetPreviousInvoices',
+      variables: {},
+      query:
+        'query GetPreviousInvoices {\nuser {\ninformationsInvoices {\ninvoices {\ndescription\ndate\namount\namountWithoutDiscount\nstatus\ndownloadUrl\nfailureReason\n}\n}\n}\n}\n'
+    },
+    resolveWithFullResponse: true
+  })
+  authResp =
+    authResp.request.response.body.data.user.informationsInvoices.invoices
+
+  authResp = authResp.map(authResp => ({
+    ...authResp,
     date: new Date(),
     currency: 'EUR',
-    filename: `${utils.formatDate(new Date())}_${VENDOR}_${doc.amount.toFixed(
-      2
-    )}EUR${doc.vendorRef ? '_' + doc.vendorRef : ''}.jpg`,
+    filename: `${authResp.date}_${VENDOR}_${authResp.amount}EUR${
+      authResp.vendorRef ? '_' + authResp.vendorRef : ''
+    }.pdf`,
     vendor: VENDOR
   }))
-}
 
-// Convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
+  //  log('info',authResp)
+  const downloadFiles = []
+  for (let i = 0, l = authResp.length; i < l; i++) {
+    const fileToDownload = {
+      fileurl: authResp[i].downloadUrl,
+      filename: authResp[i].filename
+    }
+    downloadFiles.push(fileToDownload)
+  }
+  return { authResp, downloadFiles }
 }
