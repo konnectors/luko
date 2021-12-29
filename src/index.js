620ec3558e4d2536781a6939904b22e804096613
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2021-12-29 15:36:57 +0100

    feat: parsing documents

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const {
   BaseKonnector,
   requestFactory,
   log,
-  utils,
-  cozyClient
+  utils
+  // cozyClient
 } = require('cozy-konnector-libs')
 
-const models = cozyClient.new.models
-const { Qualification } = models.document
+// const models = cozyClient.new.models
+// const { Qualification } = models.document
 
 const request = requestFactory({
   debug: false,
@@ -20,7 +20,7 @@ const request = requestFactory({
   jar: true
 })
 
-const VENDOR = 'Luko'
+const VENDOR = 'luko'
 const baseUrl = 'https://fr.luko.eu'
 
 module.exports = new BaseKonnector(start)
@@ -28,20 +28,19 @@ module.exports = new BaseKonnector(start)
 async function start(fields, cozyParameters) {
   log('info', 'Authenticating ...')
   if (cozyParameters) log('debug', 'Found COZY_PARAMETERS')
-
   await authenticate.bind(this)(fields.login, fields.password)
 
   log('info', 'Successfully logged in')
 
+  log('info', 'Fetching the list of bills')
+  const bills = await parseBills()
+  log('info', 'Parsing list of bills')
+
   log('info', 'Fetching the list of documents')
-
-  const documents = await parseBills()
-
-  log('info', 'Parsing list of documents')
-
-  log('info', 'Saving data to Cozy')
-
-  await this.saveFiles(documents.downloadFiles, fields, {
+  const documents = await parseDocuments()
+  log('info', documents)
+  log('info', 'Saving bills data to Cozy')
+  await this.saveBills(bills.downloadFiles, fields, {
     linkBankOperations: false,
     identifiers: ['Luko'],
     sourceAccount: this.accountId,
@@ -74,59 +73,86 @@ async function authenticate(username, password) {
 async function parseBills() {
   request(`${baseUrl}/my-account/invoices`)
 
-  let authResp
-  authResp = await request({
+  let billsDocs
+  billsDocs = await request({
     url: `https://cerbere.luko.eu/v1?lang=fr`,
     method: 'POST',
     json: {
       operationName: 'GetPreviousInvoices',
       variables: {},
       query:
-        'query GetPreviousInvoices {\nuser {\ninformationsInvoices {\ninvoices {\ndescription\ndate\namount\namountWithoutDiscount\nstatus\ndownloadUrl\nfailureReason\n}\n}\n}\n}\n'
+        'query GetPreviousInvoices{\nuser{\ninformationsInvoices{\ninvoices{\ndescription\ndate\namount\namountWithoutDiscount\nstatus\ndownloadUrl\nfailureReason\n}\n}\n}\n}\n'
     },
     resolveWithFullResponse: true
   })
-  authResp =
-    authResp.request.response.body.data.user.informationsInvoices.invoices
+  billsDocs =
+    billsDocs.request.response.body.data.user.informationsInvoices.invoices
 
-  authResp = authResp.map(authResp => ({
-    ...authResp,
+  billsDocs = billsDocs.map(billsDocs => ({
+    ...billsDocs,
     date: new Date(),
-    issueDate: authResp.date,
+    issueDate: billsDocs.date,
     currency: 'EUR',
-    filename: `${utils.formatDate(authResp.date)}_${VENDOR}_${
-      authResp.amount
-    }EUR${authResp.vendorRef ? '_' + authResp.vendorRef : ''}.pdf`,
+    filename: `${utils.formatDate(billsDocs.date)}_${VENDOR}_${(
+      billsDocs.amount / 100
+    ).toFixed(2)}EUR.pdf`,
     vendor: VENDOR
   }))
-
+  log('info', (billsDocs[1].amount / 100).toFixed(2))
   const downloadFiles = []
-  for (let i = 0, l = authResp.length; i < l; i++) {
-    const stringyfiedAmount = `${authResp[i].amount}`
+  for (let i = 0, l = billsDocs.length; i < l; i++) {
     const fileToDownload = {
-      currency: authResp[i].currency,
-      amount: `${
-        stringyfiedAmount.length > 2
-          ? stringyfiedAmount.slice(0, 2) + '_' + stringyfiedAmount.slice(2, 4)
-          : stringyfiedAmount
-      }`,
-      date: authResp[i].date,
-      fileurl: authResp[i].downloadUrl,
-      filename: authResp[i].filename,
+      currency: billsDocs[i].currency,
+      amount: `${(billsDocs[i].amount / 100).toFixed(2)}`,
+      date: billsDocs[i].issueDate,
+      fileurl: billsDocs[i].downloadUrl,
+      filename: billsDocs[i].filename,
       vendor: VENDOR,
       fileAttributes: {
         metadata: {
           contentAuthor: 'fr-luko.eu',
-          issueDate: utils.formatDate(authResp[i].issueDate),
-          datetime: utils.formatDate(authResp[i].date),
+          issueDate: utils.formatDate(billsDocs[i].issueDate),
+          datetime: utils.formatDate(billsDocs[i].date),
           datetimeLabel: `issueDate`,
           isSubscription: true,
-          carbonCopy: true,
-          qualification: Qualification.getByLabel('house_insurance')
+          carbonCopy: true
+          // qualification: Qualification.getByLabel('house_insurance')
         }
       }
     }
     downloadFiles.push(fileToDownload)
   }
   return { downloadFiles }
+}
+
+async function parseDocuments() {
+  request(`${baseUrl}/my-account/documents`)
+
+  let docs
+  docs = await request({
+    url: `https://cerbere.luko.eu/v1?lang=fr`,
+    method: 'POST',
+    json: {
+      operationName: 'GetContractDocuments',
+      variables: {},
+      query:
+        'query GetContractDocuments{\ncontracts{\nid\nname\nproductCode\nstate\ndocuments{\nicon\ntitle\ndescription\nurl\ntrackingCode\n}\nusers {\nid\nfirstname\nlastname\ninsuranceRole\ninsuranceDocuments{\nicon\ntitle\ndescription\nurl\ntrackingCode\n}\n}\n}\n}\n'
+    },
+    resolveWithFullResponse: true
+  })
+  const contracts = docs.request.response.body.data.contracts
+
+  const { documents: documents, users: users } = contracts[0]
+
+  const docResult = []
+  for (const doc of documents) {
+    docResult.push(doc)
+  }
+  log('info', docResult)
+
+  const userResult = []
+  for (const user of users) {
+    userResult.push(...user.insuranceDocuments)
+  }
+  log('info', userResult)
 }
